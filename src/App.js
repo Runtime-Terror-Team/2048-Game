41bcfb490c47bf625811991e7e4e1252eb47707b
Maseera Ahmed <madihaahmed2017@gmail.com>
2021-08-02 13:52:49 +0530

    Front page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import BoardView from "./components/Board";
 import React from "react";
+import Front from "./Front-pg/Front.jsx";
+
 
 function App() {
   return (
     <div className="App">
+      <Front />
       <BoardView />
     </div>
   );
